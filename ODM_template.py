e9d97a7c41d23beb21a4b4812226e86009473722
dubiefe <emiliedubief@free.fr>
2025-10-01 13:02:06 +0200

    Initial Commit

---
+++ ODM_template.py
@@ -0,0 +1,331 @@
+__author__ = '???'
+__students__ = '???'
+
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+import time
+from typing import Generator, Any, Self
+from geojson import Point
+import pymongo
+from bson.objectid import ObjectId
+import yaml
+
+def getLocationPoint(address: str) -> Point:
+    """
+    Gets the coordinates of an address in geojson.Point format.
+    Uses the geopy API to obtain the coordinates of the address.
+    Be careful, the API is public and has a request limit, use sleeps.
+
+    Parameters
+    ----------
+    address : str
+        Full address from which to obtain coordinates
+
+    Returns
+    -------
+    geojson.Point
+        Coordinates of the address point
+    """
+    location = None
+    while location is None:
+        try:
+            time.sleep(1)
+            # TODO
+            # A user_agent is required to use the API
+            # Use a random name for the user_agent
+            location = Nominatim(user_agent="My-Random-Name").geocode(address)
+        except GeocoderTimedOut:
+            # May throw an exception if timeout is exceeded
+            # Try again
+            continue
+    # TODO
+
+class Model:
+    """
+    Abstract model class.
+    Create as many classes inheriting from this class as
+    collections/models desired in the database.
+
+    Attributes
+    ----------
+    required_vars : set[str]
+        Set of attributes required by the model
+    admissible_vars : set[str]
+        Set of attributes allowed by the model
+    db : pymongo.collection.Collection
+        Connection to the database collection
+
+    Methods
+    -------
+    __setattr__(name: str, value: str | dict) -> None
+        Overrides the attribute assignment method to control
+        which attributes are modified and when.
+    __getattr__(name: str) -> Any
+        Overrides the attribute access method
+    save() -> None
+        Saves the model to the database
+    delete() -> None
+        Deletes the model from the database
+    find(filter: dict[str, str | dict]) -> ModelCursor
+        Performs a read query in the DB.
+        Returns a ModelCursor of models
+    aggregate(pipeline: list[dict]) -> pymongo.command_cursor.CommandCursor
+        Returns the result of an aggregate query.
+    find_by_id(id: str) -> dict | None
+        Searches for a document by its ID using cache and returns it.
+        If not found, returns None.
+    init_class(db_collection: pymongo.collection.Collection, required_vars: set[str], admissible_vars: set[str]) -> None
+        Initializes class variables during system initialization.
+    """
+    _required_vars: set[str]
+    _admissible_vars: set[str]
+    _location_var: None
+    _db: pymongo.collection.Collection
+    _data: dict[str, str | dict] = {}
+
+    def __init__(self, **kwargs: dict[str, str | dict]):
+        """
+        Initializes the model with values provided in kwargs.
+        Checks that the values provided in kwargs are allowed
+        by the model and that required attributes are provided.
+
+        Parameters
+        ----------
+        kwargs : dict[str, str | dict]
+            Dictionary with the model's attribute values
+        """
+        # TODO
+        # Perform necessary checks and handling
+        # before assignment.
+        # Assign all values in kwargs to attributes with
+        # names matching the keys in kwargs
+        # Use the data attribute to store variables
+        # saved in the database in a single attribute
+        # Encapsulating data in one variable simplifies
+        # handling in methods like save.
+        self._data.update(kwargs)
+
+    def __setattr__(self, name: str, value: str | dict) -> None:
+        """
+        Overrides the attribute assignment method to control
+        which attributes are modified and when.
+        """
+        # TODO
+        # Perform necessary checks and handling
+        # before assignment.
+        # Assign the value to the variable name
+        self._data[name] = value
+
+    def __getattr__(self, name: str) -> Any:
+        """
+        Overrides the attribute access method.
+        __getattr__ is only called when the attribute
+        is not found in the object.
+        """
+        if name in {'_modified_vars', '_required_vars', '_admissible_vars', '_db', '_data', '_location_var'}:
+            return super().__getattribute__(name)
+        try:
+            return self._data[name]
+        except KeyError:
+            raise AttributeError
+
+    def save(self) -> None:
+        """
+        Saves the model to the database.
+        If the model does not exist in the database, a new
+        document is created with the model's values. Otherwise,
+        the existing document is updated with the new values.
+        """
+        # TODO
+        pass  # Don't forget to remove this line once implemented
+
+    def delete(self) -> None:
+        """
+        Deletes the model from the database.
+        """
+        # TODO
+        pass
+
+    @classmethod
+    def find(cls, filter: dict[str, str | dict]) -> Any:
+        """
+        Uses pymongo's find method to perform a read query
+        in the database.
+        find should return a ModelCursor of models.
+
+        Parameters
+        ----------
+        filter : dict[str, str | dict]
+            Dictionary with the search criteria
+
+        Returns
+        -------
+        ModelCursor
+            Cursor of models
+        """
+        # TODO
+        # cls is the pointer to the class
+        pass  # Don't forget to remove this line once implemented
+
+    @classmethod
+    def aggregate(cls, pipeline: list[dict]) -> pymongo.command_cursor.CommandCursor:
+        """
+        Returns the result of an aggregate query.
+        Nothing needs to be done in this function.
+        It will be used for queries requested
+        in the second project of the practice.
+
+        Parameters
+        ----------
+        pipeline : list[dict]
+            List of stages in the aggregate query
+
+        Returns
+        -------
+        pymongo.command_cursor.CommandCursor
+            pymongo cursor with the query result
+        """
+        return cls.db.aggregate(pipeline)
+
+    @classmethod
+    def find_by_id(cls, id: str) -> Self | None:
+        """
+        DO NOT IMPLEMENT UNTIL THE THIRD PROJECT
+        Searches for a document by its ID using cache and returns it.
+        If not found, returns None.
+
+        Parameters
+        ----------
+        id : str
+            ID of the document to search
+
+        Returns
+        -------
+        Self | None
+            Model of the found document or None if not found
+        """
+        # TODO
+        pass
+
+    @classmethod
+    def init_class(cls, db_collection: pymongo.collection.Collection, indexes: dict[str, str], required_vars: set[str], admissible_vars: set[str]) -> None:
+        """
+        Initializes class attributes during system initialization.
+        Here, indexes should be initialized or ensured. Additional
+        initialization/checks or changes may also be made
+        as deemed necessary by the student.
+
+        Parameters
+        ----------
+        db_collection : pymongo.collection.Collection
+            Connection to the database collection
+        indexes: Dict[str, str]
+            Set of indexes and index types for the collection
+        required_vars : set[str]
+            Set of attributes required by the model
+        admissible_vars : set[str]
+            Set of attributes allowed by the model
+        """
+        cls._db = db_collection
+        cls._required_vars = required_vars
+        cls._admissible_vars = admissible_vars
+        # TODO
+        
+class ModelCursor:
+    """
+    Cursor to iterate over the documents resulting from a query.
+    Documents must be returned as model objects.
+
+    Attributes
+    ----------
+    model_class : Model
+        Class used to create models from the documents being iterated.
+    cursor : pymongo.cursor.Cursor
+        Pymongo cursor to iterate
+
+    Methods
+    -------
+    __iter__() -> Generator
+        Returns an iterator that goes through the cursor elements
+        and returns the documents as model objects.
+    """
+    def __init__(self, model_class: Model, cursor: pymongo.cursor.Cursor):
+        """
+        Initializes the cursor with the model class and pymongo cursor.
+
+        Parameters
+        ----------
+        model_class : Model
+            Class used to create models from the documents being iterated.
+        cursor: pymongo.cursor.Cursor
+            Pymongo cursor to iterate
+        """
+        self.model = model_class
+        self.cursor = cursor
+
+    def __iter__(self) -> Generator:
+        """
+        Returns an iterator that goes through the cursor elements
+        and returns the documents as model objects.
+        Use yield to generate the iterator.
+        Use next to get the next document from the cursor.
+        Use alive to check if more documents exist.
+        """
+        # TODO
+        pass  # Don't forget to remove this line once implemented
+    
+def initApp(definitions_path: str = "./models.yml", mongodb_uri="mongodb://localhost:27017/", db_name="abd", scope=globals()) -> None:
+    """
+    Declares the classes that inherit from Model for each of the
+    models of the collections defined in definitions_path.
+    Initializes the model classes by providing the indexes and
+    allowed and required attributes for each of them, and the connection to the
+    database collection.
+
+    Parameters
+    ----------
+    definitions_path : str
+        Path to the model definitions file
+    mongodb_uri : str
+        URI for connecting to the database
+    db_name : str
+        Name of the database
+    """
+    # TODO
+    # Initialize database
+
+    # TODO
+    # Declare as many model classes as there are collections in the database
+    # Read the model definitions file to get the collections,
+    # indexes, and the allowed and required attributes for each of them.
+
+    # Example of model declaration for a collection called MyModel
+    scope["MyModel"] = type("MyModel", (Model,), {})
+
+    # Ignore Pylance warning about MyModel, it cannot detect
+    # that the class was declared in the previous line since it is done
+    # at runtime.
+    
+if __name__ == '__main__':
+    # Initialize database and models with initApp
+    # TODO
+    initApp()
+
+    # Example
+    m = MyModel(name="Pablo", surname="Ramos", age=18)
+    m.save()
+    m.name = "Pedro"
+    print(m.name)
+
+    # Run tests to verify the model works correctly
+    # TODO
+    # Create model
+    # Assign new value to allowed variable of the object
+    # Assign new value to disallowed variable of the object
+    # Save
+    # Assign new value to allowed variable of the object
+    # Save
+    # Search for new document with find
+    # Get first document
+    # Modify value of allowed variable
+    # Save
